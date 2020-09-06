d2694448334837db9aa78b332977652c83e6ba79
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-09-06 13:21:59 +0100

    Add weapons page

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,37 @@ module.exports = {
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
+        {
+            // Querying to a SQLite database
+            resolve: `gatsby-source-sql`,
+            options: {
+                typeName: "MhwWeaponData",
+                // This is the field under which the data will be accessible in a future version
+                fieldName: "mhwWeaponData",
+                dbEngine: {
+                    client: "sqlite3",
+                    connection: {
+                        filename: "./data/mhw.db",
+                    },
+                    useNullAsDefault: true,
+                },
+                // SELECT weapon.id, weapon_type, weapon_text.name AS WeaponName, lang_id
+                // FROM weapon
+                // INNER JOIN weapon_text ON weapon_text.id = weapon.id
+                // WHERE weapon_text.lang_id = "en"
+                queryChain: function (x) {
+                    return x
+                        .select(
+                            "weapon.id as WeaponId",
+                            "weapon_type as WeaponType",
+                            "weapon_text.name as WeaponName",
+                            "lang_id as LangId"
+                        )
+                        .from("weapon")
+                        .innerJoin("weapon_text", "weapon_text.id", "weapon.id")
+                        .where("weapon_text.lang_id", "=", "en");
+                },
+            },
+        },
     ],
 };
