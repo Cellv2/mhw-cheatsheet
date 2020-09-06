1f9d75fde3c4052d8d16187f60f697cdbdb86d49
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-09-06 20:13:22 +0100

    Add remainder of POC pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,5 +62,90 @@ module.exports = {
                 },
             },
         },
+        {
+            // Querying to a SQLite database
+            resolve: `gatsby-source-sql`,
+            options: {
+                typeName: "MhwArmourData",
+                // This is the field under which the data will be accessible in a future version
+                fieldName: "mhwArmourData",
+                dbEngine: {
+                    client: "sqlite3",
+                    connection: {
+                        filename: "./data/mhw.db",
+                    },
+                    useNullAsDefault: true,
+                },
+                // SELECT armor.id, armor_type, armor_text.name AS armorName, lang_id
+                // FROM armor
+                // INNER JOIN armor_text ON armor_text.id = armor.id
+                // WHERE armor_text.lang_id = "en"
+                queryChain: function (x) {
+                    return x
+                        .select(
+                            "armor.id as ArmourId",
+                            "armor_type as ArmourType",
+                            "armor_text.name as ArmourName",
+                            "lang_id as LangId"
+                        )
+                        .from("armor")
+                        .innerJoin("armor_text", "armor_text.id", "armor.id")
+                        .where("armor_text.lang_id", "=", "en");
+                },
+            },
+        },
+        {
+            // Querying to a SQLite database
+            resolve: `gatsby-source-sql`,
+            options: {
+                typeName: "MhwMonsterData",
+                // This is the field under which the data will be accessible in a future version
+                fieldName: "mhwMonsterData",
+                dbEngine: {
+                    client: "sqlite3",
+                    connection: {
+                        filename: "./data/mhw.db",
+                    },
+                    useNullAsDefault: true,
+                },
+                // SELECT
+                //     monster.id,
+                //     size,
+                //     monster_text.name,
+                //     monster_text.lang_id AS MonsterLangId,
+                //     monster_text.description,
+                //     location_text.name,
+                //     location_text.lang_id AS LocationLangId,
+                //     monster_habitat.start_area,
+                //     monster_habitat.move_area,
+                //     monster_habitat.rest_area
+                // FROM monster
+                // INNER JOIN monster_text ON monster_text.id = monster.id
+                // INNER JOIN monster_habitat ON monster_habitat.monster_id = monster.id
+                // INNER JOIN location_text ON location_text.id = monster_habitat.location_id
+                // WHERE monster_text.lang_id = "en"  AND location_text.lang_id = "en"
+                queryChain: function (x) {
+                    return x
+                        .select(
+                            "monster.id AS MonsterId",
+                            "size AS MonsterSize",
+                            "monster_text.name AS MonsterName",
+                            "monster_text.lang_id AS MonsterLangId",
+                            "monster_text.description AS MonsterDescription",
+                            "location_text.name AS LocationName",
+                            "location_text.lang_id AS LocationLangId",
+                            "monster_habitat.start_area AS MonsterStartArea",
+                            "monster_habitat.move_area AS MonsterMoveArea",
+                            "monster_habitat.rest_area AS MonsterRestArea",
+                        )
+                        .from("monster")
+                        .innerJoin("monster_text", "monster_text.id", "monster.id")
+                        .innerJoin("monster_habitat", "monster_habitat.monster_id", "monster.id")
+                        .innerJoin("location_text", "location_text.id", "monster_habitat.location_id")
+                        .where("monster_text.lang_id", "=", "en")
+                        .where("location_text.lang_id", "=", "en")
+                },
+            },
+        },
     ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,10 @@ const IndexPage = () => (
         <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
         <br />
         <Link to="/weapons-page/">Go to "MHW Weapons Page"</Link>
+        <br />
+        <Link to="/armour-page/">Go to "MHW Armour Page"</Link>
+        <br />
+        <Link to="/monsters-page/">Go to "MHW Monsters Page"</Link>
     </Layout>
 );
 
