24e97dea24f920e10e14f5a93c39a6bbd607ce7a
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-09-13 13:37:28 +0100

    Separate table into own component file

--- src/pages/react-table-test-page.tsx
+++ src/pages/react-table-test-page.tsx
@@ -4,7 +4,7 @@ import { PageProps, Link, graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import { useTable, TableOptions } from "react-table";
+import Table from "../components/Table";
 
 type DataProps = {
     site: {
@@ -29,7 +29,7 @@ type DataProps = {
     };
 };
 
-const ReactTableTestPage: React.FC<PageProps<DataProps>> = ({ data, path }) => { 
+const ReactTableTestPage: React.FC<PageProps<DataProps>> = ({ data, path }) => {
     // Header is what will be displayed
     // accessor needs to be the key one of the GQL nodes that's passed in
     const tableColumns = React.useMemo(
@@ -61,69 +61,10 @@ const ReactTableTestPage: React.FC<PageProps<DataProps>> = ({ data, path }) => {
         });
     }, []);
 
-
-    const tableConfig: TableOptions<{}> = {
-        columns: tableColumns,
-        data: tableData,
-    };
-    const tableInstance = useTable(tableConfig);
-
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = tableInstance;
-
     return (
         <Layout>
             <SEO title="Using React-Table" />
-            {/* apply the table props */}
-            <table {...getTableProps()} style={{ border: "solid 1px blue" }}>
-                <thead>
-                    {headerGroups.map((headerGroup) => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                                <th
-                                    {...column.getHeaderProps()}
-                                    style={{
-                                        borderBottom: "solid 3px red",
-                                        background: "aliceblue",
-                                        color: "black",
-                                        fontWeight: "bold",
-                                    }}
-                                >
-                                    {column.render("Header")}
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
-                        prepareRow(row);
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map((cell) => {
-                                    return (
-                                        <td
-                                            {...cell.getCellProps()}
-                                            style={{
-                                                padding: "10px",
-                                                border: "solid 1px gray",
-                                                background: "papayawhip",
-                                            }}
-                                        >
-                                            {cell.render("Cell")}
-                                        </td>
-                                    );
-                                })}
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+            <Table tableColumns={tableColumns} tableData={tableData}></Table>
             <Link to="/">Go back to the homepage</Link>
         </Layout>
     );
