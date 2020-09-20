a0dd985240ac80170a3708ca11e03af5a86aaa17
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-09-20 21:03:17 +0100

    :truck: Rename to match new export name

--- src/pages/react-table-test-page.tsx
+++ src/pages/react-table-test-page.tsx
@@ -4,7 +4,7 @@ import { PageProps, Link, graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import FilterableTable from "../components/Table/Table";
+import FilterableTable from "../components/Table/FilterableTable";
 
 type DataProps = {
     site: {
