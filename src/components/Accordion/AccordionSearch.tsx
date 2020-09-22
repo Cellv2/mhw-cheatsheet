bed8bee7b089bfec3dc75c7d8d89695a522b80d0
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-09-22 23:26:58 +0100

    Switch to refs and cleanup

--- src/components/Accordion/AccordionSearch.tsx
+++ src/components/Accordion/AccordionSearch.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Fuse from "fuse.js";
 
 import { bookData, book } from "./data";
@@ -9,43 +9,32 @@ type Props = {
 };
 
 const AccordionSearch = (props: Props) => {
+    const fuseRef = useRef<Fuse<{}>>();
     const [searchQuery, setSearchQuery] = useState<string>("");
-    // const [queryResults, setQueryResults] = useState<book[]>(bookData.books);
 
-    const fuse = new Fuse(bookData.books, {
-        keys: ["title", "author", "isbn"],
-    });
+    useEffect(() => {
+        const fuse = new Fuse(bookData.books, {
+            keys: ["title", "author", "isbn"],
+        });
+
+        fuseRef.current = fuse;
+    }, []);
 
     const handleInputOnChange = (
         event: React.ChangeEvent<HTMLInputElement>
     ): void => {
         setSearchQuery(event.target.value);
 
-        if (!event.target.value || event.target.value === "") {
-            // setQueryResults(bookData.books);
+        if (!event.target.value || event.target.value === "" || !fuseRef.current) {
             props.updateQueryResults(props.initialDataset);
         } else {
-            const search = fuse.search(event.target.value);
+            const search = fuseRef.current.search(event.target.value);
             const data = search.map((item) => item.item);
-            // setQueryResults(data);
             props.updateQueryResults(data);
         }
     };
 
-    return (
-        <>
-            <input onChange={handleInputOnChange} value={searchQuery} />
-            {/* {queryResults.map((result) => {
-                return (
-                    <div key={result.isbn}>
-                        <p>ISBN: {result.isbn}</p>
-                        <p>Title: {result.title}</p>
-                        <p>Author: {result.author}</p>
-                    </div>
-                );
-            })} */}
-        </>
-    );
+    return <input onChange={handleInputOnChange} value={searchQuery} />;
 };
 
 export default AccordionSearch;
